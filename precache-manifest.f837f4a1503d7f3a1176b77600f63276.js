e84e6df423baebcae776daa3d6bdd79d33355bce
Mike Carter <mike@weareleaf.com>
2019-04-06 14:15:52 +0100

    Updates

---
+++ precache-manifest.f837f4a1503d7f3a1176b77600f63276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92d32c66160d5f703b02ab0f3d1cb01d",
+    "url": "/static/media/logo.92d32c66.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9b069b442ba3db8c3424",
+    "url": "/static/js/main.9b069b44.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.10534ad0.chunk.js"
+  },
+  {
+    "revision": "9b069b442ba3db8c3424",
+    "url": "/static/css/main.1e873c3d.chunk.css"
+  },
+  {
+    "revision": "47115cd07a0b101d128b2109bb45a962",
+    "url": "/index.html"
+  }
+];
